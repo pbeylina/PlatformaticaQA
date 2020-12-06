e415f077d9a6651cb2526c3e97f1006e8729d114
Elena Grinenko <lenagrinenko.us@gmail.com>
2020-12-06 15:41:38 -0800

    Create lenaHW13Test

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -5,6 +5,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
+import static java.lang.Thread.sleep;
+
 public class SimpleTest extends BaseTest {
 
     @Test
@@ -16,7 +18,7 @@ public class SimpleTest extends BaseTest {
 
         Assert.assertEquals(name.getText(), "PlatformaticaQA");
 
-        Thread.sleep(3000);
+        sleep(3000);
     }
 
     @Test
@@ -31,5 +33,33 @@ public class SimpleTest extends BaseTest {
         WebElement input = driver.findElement(By.xpath("(//div[@class='input-group']/input)[1]"));
         Assert.assertEquals(input.getAttribute("value"), "https://github.com/SergeiDemyanenko/PlatformaticaQA.git");
     }
+    @Test
+    public void lenaHW13Test() throws InterruptedException {
 
-}
+        WebDriver browser = getDriver();
+        browser.get("https://coderoad.ru/48259191/%D0%92-Selenium-%D0%BA%D0%B0%D0%BA-%D0%BF%D1%80%D0%BE%D0%B2%D0%B5%D1%80%D0%B8%D1%82%D1%8C-%D1%87%D1%82%D0%BE-%D0%BF%D1%80%D0%B8-%D0%BD%D0%B0%D0%B6%D0%B0%D1%82%D0%B8%D0%B8-%D0%BD%D0%B0-%D1%8D%D0%BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82-%D0%BC%D1%8B-%D1%84%D0%B0%D0%BA%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8-%D0%BF%D0%B5%D1%80%D0%B5%D1%88%D0%BB%D0%B8-%D0%BD%D0%B0");
+        WebElement name = browser.findElement(By.xpath("//a[contains(text(),'О нас')]"));
+
+        Assert.assertEquals(name.getText(), "О нас");
+
+        sleep(3000);
+
+        name.click();
+
+        Assert.assertEquals(browser.getCurrentUrl(),"https://coderoad.ru/about.html");
+
+
+    }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
