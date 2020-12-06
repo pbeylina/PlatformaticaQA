598ea547a8e03ecbba782b81a6beabec30f95098
DenHubPy <dkim.qa@gmail.com>
2020-12-06 05:37:51 -0800

    add XPath test

--- src/test/java/GroupOfBugKillers.java
+++ src/test/java/GroupOfBugKillers.java
@@ -8,7 +8,7 @@ import runner.BaseTest;
 public class GroupOfBugKillers extends BaseTest {
 
     @Test
-    public void DenTest() throws InterruptedException {
+    public void denSpeedTest() throws InterruptedException {
 
         WebDriver driver = getDriver();
         driver.get("https://www.speedtest.net/");
@@ -19,4 +19,23 @@ public class GroupOfBugKillers extends BaseTest {
         Thread.sleep(35000);
     }
 
+    @Test
+    public void denXPathTest() throws InterruptedException {
+        WebDriver driver = getDriver();
+        driver.get("https://github.com/SergeiDemyanenko/PlatformaticaQA");
+
+        Thread.sleep(2000);
+        WebElement button = driver.findElement(By.xpath("//a[normalize-space()='Sign in']"));
+        button.click();
+        WebElement userNameField = driver.findElement(By.xpath("//input[@id='login_field']"));
+        userNameField.sendKeys("userName");
+        WebElement passField = driver.findElement(By.xpath("//input[@id='password']"));
+        passField.sendKeys("password");
+        Thread.sleep(2000);
+        WebElement signInButton = driver.findElement(By.xpath("//input[@value='Sign in']"));
+        signInButton.click();
+
+        WebElement pullRequestsButton = driver.findElement(By.xpath("//a[@aria-label='Pull requests you created']"));
+        pullRequestsButton.click();
+    }
 }
